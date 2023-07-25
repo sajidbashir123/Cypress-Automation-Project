2345e8b313d9f469a58da76cdb589b1dcd029ad8
sajidbashir123 <sajidbashir278@gmail.com>
2023-07-25 12:31:28 +0500

    Fixed issues in environment variable files and adding screenshots and vedio

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,17 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "f5u2h8",
   e2e: {
     baseUrl: "https://dev.meltzerhellrung.com",
     watchForFileChanges: false,
-    defaultCommandTimeout: (20000),
-
+    defaultCommandTimeout: 20000,
 
     setupNodeEvents(on, config) {
-
       // implement node event listeners here
     },
+  },
+  env: {
+    url: "https://rahulshettyacademy.com/angularpractice/",
   },
 });
