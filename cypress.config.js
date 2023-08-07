8770535b7ba9e7fee5471cb25db5e6dd34ba0465
sajidbashir123 <sajidbashir278@gmail.com>
2023-08-07 11:27:24 +0500

    Added mochaawesome dependency to genrating the HTML reported for external uses

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,18 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: "f5u2h8",
+  projectId: "f5u2h8", //adding project ID for the use of Cypress cloud
+
   e2e: {
     baseUrl: "https://dev.meltzerhellrung.com",
-    watchForFileChanges: false,
+    watchForFileChanges: false, // Not run automatically after any file changes
     defaultCommandTimeout: 20000,
-
+    reporter: "mochawesome", // To generate the HTML report of the executed tested case for external use. it added when we install the mochaawesome dependency
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
+
   env: {
     url: "https://rahulshettyacademy.com/angularpractice/",
   },
