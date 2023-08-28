6eb6724108757a680a2aeb6e1727d6e10b2a0b93
sajidbashir123 <sajidbashir278@gmail.com>
2023-08-28 17:43:21 +0500

    add a file of MeltzerHellrungDropdown file where a dropdown is automatically control

--- cypress/e2e/5-alerts&popup.cy.js
+++ cypress/e2e/5-alerts&popup.cy.js
@@ -1,25 +1,22 @@
 /// <reference types="Cypress" />
 
-
-describe('My Automation Project', () => {
-
-  it('Verifying the Web alert and popup text', () => {
-    cy.visit('https://rahulshettyacademy.com/AutomationPractice/');
-    cy.get('#alertbtn').click();
-    cy.get('#confirmbtn').click();
+describe("My Automation Project", () => {
+  it("Verifying the Web alert and popup text", () => {
+    cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
+    cy.get("#alertbtn").click();
+    cy.get("#confirmbtn").click();
 
     //popup alert
-    cy.on('window:alert', (str) => {
-
+    cy.on("window:alert", (str) => {
       //its comes from Mocha
-      expect(str).to.be.equal('Hello , share this practice page and share your knowledge');
-    })
+      expect(str).to.be.equal(
+        "Hello , share this practice page and share your knowledge"
+      );
+    });
 
     //Confirmation alert
-    cy.on('window:confirm', (str) => {
-      expect(str).to.be.equal('Hello , Are you sure you want to confirm?');
-    })
-
-
-  })
-})
+    cy.on("window:confirm", (str) => {
+      expect(str).to.be.equal("Hello , Are you sure you want to confirm?");
+    });
+  });
+});
